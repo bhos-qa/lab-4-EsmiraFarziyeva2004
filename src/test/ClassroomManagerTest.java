26b02282da2b17f4c3cd4f9b868107c7d13f48db
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-29 14:37:58 +0400

    Update ClassroomManagerTest.java

--- src/test/ClassroomManagerTest.java
+++ src/test/ClassroomManagerTest.java
@@ -12,37 +12,19 @@ class ClassroomManagerTest {
 
     @Test
     void testAddStudent() {
-        classroomManager.addStudent();
-        assertEquals(1, classroomManager.getTotalStudents(), "Total students should be 1 after adding a student.");
-    }
-
-    @Test
-    void testRemoveStudent() {
-        classroomManager.addStudent(); // First, add a student
-        classroomManager.removeStudent();
-        assertEquals(0, classroomManager.getTotalStudents(), "Total students should be 0 after removing a student.");
-    }
-
-    @Test
-    void testGetTotalStudents() {
-        assertEquals(0, classroomManager.getTotalStudents(), "Total students should be initially 0.");
-        classroomManager.addStudent();
-        assertEquals(1, classroomManager.getTotalStudents(), "Total students should be 1 after adding a student.");
-    }
-
-    @Test
-    void testCreateClass() {
-        classroomManager.createClass();
-        assertEquals(1, classroomManager.getTotalClasses(), "Total classes should be 1 after creating a class.");
-    }
-
-    @Test
-    void testCalculateStudentToClassRatio() {
-        classroomManager.addStudents(10); // Adding 10 students
-        classroomManager.createClass(); // Creating 1 class
-        assertEquals(10.0, classroomManager.calculateStudentToClassRatio(), "The student-to-class ratio should be 10.0.");
+        // Initially, the total number of students should be 0
+        assertEquals(0, classroomManager.getTotalStudents(), "Total students should initially be 0.");
         
-        classroomManager.removeClass(); // Remove class
-        assertEquals(0.0, classroomManager.calculateStudentToClassRatio(), "The student-to-class ratio should be 0.0 when there are no classes.");
+        // Add a student
+        classroomManager.addStudent();
+        
+        // Now, the total number of students should be 1
+        assertEquals(1, classroomManager.getTotalStudents(), "Total students should be 1 after adding one student.");
+        
+        // Add another student
+        classroomManager.addStudent();
+        
+        // Now, the total number of students should be 2
+        assertEquals(2, classroomManager.getTotalStudents(), "Total students should be 2 after adding another student.");
     }
 }
